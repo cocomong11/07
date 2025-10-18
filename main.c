bd0a2efb2ca84934e5b2357c75406367f52eb05e
cocomong11 <carpediem1011@naver.com>
2025-10-18 23:52:15 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,17 +3,16 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int inc(int counter);
+int counter ;
+
+void set_counter(){
+	counter=20;
+}
 int main(int argc, char *argv[]) {
-	int i=10;
-	printf("함수 호출전 i=%d\n",i);
-	i=inc(i);// 반환되는 값을 i에 다시 넣어줌 
-	printf("함수 호출후 i=%d\n",i);
+	
+	printf("counter=%d\n",counter);
+	set_counter();
+	printf("counter=%d\n",counter);
 	return 0;
 }
-//counter자체가 함수 지역변수기 때문에 반환이 돼도 메인에 반영이 안됨(counter가 아니라 i를 써도 겉으로 보기에만 같은 이름인 다른 변수이기  떄문에 반영안됨 
-int inc(int counter){
-	counter++;
-	return counter;
 
-}
